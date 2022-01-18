f9db715ec270f5dd89b2683d8256aa5b4c8c7477
Callum Macdonald <git@callum-macdonald.com>
2022-01-18 15:05:48 +0100

    Simple script. Seems to work.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # joplin2dendron
+
+NOTE: This is FYI-open-source code. It's shared for your information, feel free
+to use / fork / update it to your heart's content. But do not expect free
+support, bug fixes, or otherwise.
+
+## Usage
+
 Helper to process imported Joplin notes in Dendron.
+
+- Export all your Joplin notes like File > Export All > Markdown + Front Matter
+- Import these notes into Dendron
+  - As at 18 Jan 2022 you need to build Dendron from source on the
+  `chore/import-frontmatter-on-markdown-import` branch. That should not be
+  necessary after the pull request is merged and released.
+  - Edit your `config.import.yml` file and add the snippet below.
+- Clone this repo, run `yarn` to install dependencies, `yarn build` to build
+- Run `node dist/index.js --dry-run --data-path ~/path/to/dendron/vault`
+  - Be sure to commit your files before running this
+  - Remove the `--dry-run` to have the script commit changes
+  - Inspect the changes made and commit / revert appropriately
+
+```yaml
+importFrontmatter: true
+frontmatterMapping:
+  updated: joplin_updated
+  created: joplin_created
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,76 @@
-console.log("Works! #lcHKb6");
+import Files, { File } from "nonplain";
+import { Command } from "commander";
+import * as path from "path";
+import * as dayjs from "dayjs";
+
+const program = new Command();
+
+program
+  .requiredOption(
+    "-d --data-path <path>",
+    "Path to the dendron folder to transform."
+  )
+  .option(
+    "--dry-run",
+    "Show the operations without actually changing any files."
+  );
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+const DATA_PATH = options.dataPath as string;
+const DRY_RUN = options.dryRun;
+
+const transformFile = (file: File) => {
+  const { body, metadata: oldMetadata } = file;
+  const { joplin_updated, joplin_created, ...rest } = oldMetadata;
+  if (
+    typeof joplin_created === "undefined" ||
+    typeof joplin_updated === "undefined"
+  ) {
+    return file;
+  }
+
+  const created = dayjs(joplin_created).valueOf();
+  const updated = dayjs(joplin_updated).valueOf();
+
+  const metadata = { ...rest, created, updated };
+
+  return { body, metadata };
+};
+
+const writeUpdatedFile = (file: File) => {
+  const {
+    metadata: {
+      file: { dir, base },
+    },
+  } = file;
+  const filepath = path.join(dir, base);
+  file.write(filepath, { encoding: "utf8" });
+};
+
+const start = async () => {
+  console.log("Starting #tKPTz4");
+  const files = new Files().load(path.join(DATA_PATH, "/*.md"));
+
+  files.transform(transformFile);
+
+  if (!DRY_RUN) {
+    const instances = files.collectInstances();
+    for (const instance of instances) {
+      writeUpdatedFile(instance);
+    }
+  } else {
+    // Figure out how to show changes
+    console.log(`Showing changes hasn't been implemented yet. #uPVimn`);
+  }
+
+  console.log("Finished. #xFn630");
+};
+
+start().catch((error) => {
+  console.error("FATAL ERROR. Program will exit. #ahct8l");
+  console.error(error);
+  process.exit(1);
+});
